3fef868aabe013e55ac3c9327db6ba29059ebfa4
Yahya Mohamed Eisa <91888199+Yahya-eisa@users.noreply.github.com>
2025-10-25 14:32:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def df_to_pdf_table(df, title="OUT LET"):
 
     table = Table(data, colWidths=col_widths[:len(df.columns)], repeatRows=1)
     table.setStyle(TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#ADD8E6")),
+        ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor("#FFE4E1")),
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
         ('GRID', (0, 0), (-1, -1), 0.25, colors.black),
         ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
@@ -213,5 +213,6 @@ if uploaded_files:
             file_name=file_name,
             mime="application/pdf"
         )
+
 
 
